7953528aa817f6ca6f9bbf8bc8ca9333d8c74b86
Sho Hashimoto <hashimoto@shokai.org>
2013-05-23 05:14:41 +0900

    update bundle and fix sinatra routes

--- config.ru
+++ config.ru
@@ -7,8 +7,10 @@ require 'sinatra/content_for'
 require 'sinatra/rocketio'
 require 'sinatra/rocketio/linda'
 require 'haml'
+require 'sass'
 require File.expand_path 'main', File.dirname(__FILE__)
 
+set :rocketio, :comet => true, :websocket => true
 set :haml, :escape_html => true
 
 run Sinatra::Application
--- main.rb
+++ main.rb
@@ -15,16 +15,17 @@ linda.on :* do |event, tuple, client|
   puts "#{event} #{tuple.tuple} in <#{tuple.space}> by <#{client}>"
 end
 
-get %r{^/([\w/]+)$} do |arr|
-  @arr = arr.split("/")
-  @space = @arr.shift
-  haml :tuple
-end
-
 get '/' do
   haml :index
 end
 
-get '/*.css' do
-  scss params[:splat][0].to_sym
+get '/*.css' do |path|
+  scss path.to_sym
 end
+
+get '/*' do |path|
+  @arr = path.split("/")
+  @space = @arr.shift
+  haml :tuple
+end
+
