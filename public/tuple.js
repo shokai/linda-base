a8d12a10c423ba027e104df8be566988733d4f9a
Sho Hashimoto <hashimoto@shokai.org>
2013-05-23 22:49:59 +0900

    editable tuple and space #4

--- public/tuple.js
+++ public/tuple.js
@@ -1,14 +1,6 @@
 var linda = new Linda();
 var ts = new linda.TupleSpace(space_name);
 
-var log = function(msg){
-  console.log(msg);
-  if(typeof msg !== "string") msg = JSON.stringify(msg);
-  var watch_msg = $("<p>").text(msg).append(" - "+ new Date().toString());
-  watch_msg.fadeIn(600);
-  $("#watch").prepend(watch_msg);
-};
-
 linda.io.on("connect", function(){
   ts.watch(tuple, function(tuple){
     log(tuple);
@@ -16,7 +8,29 @@ linda.io.on("connect", function(){
 });
 
 $(function(){
+  $("#space").editable("click", function(e){
+    space_name = e.value;
+  });
+
+  $("#tuple").editable("click", function(e){
+    try{
+      tuple = JSON.parse(e.value);
+    }
+    catch(ex){
+      alert(e.value + " is not valid Tuple");
+      e.target.html(e.old_value);
+    }
+  });
+
   $("#btn_write").click(function(e){
     ts.write(tuple);
   });
 });
+
+var log = function(msg){
+  console.log(msg);
+  if(typeof msg !== "string") msg = JSON.stringify(msg);
+  var watch_msg = $("<p>").text(msg).append(" - "+ new Date().toString());
+  watch_msg.fadeIn(600);
+  $("#watch").prepend(watch_msg);
+};
