0f48789c691d82a066662d0d5da4b395e06c1290
Sho Hashimoto <hashimoto@shokai.org>
2013-05-23 23:38:44 +0900

    edit tuple and jump #4

--- public/tuple.js
+++ public/tuple.js
@@ -10,11 +10,13 @@ linda.io.on("connect", function(){
 $(function(){
   $("#space").editable("click", function(e){
     space_name = e.value;
+    if(url() !== location.href) location.href = url();
   });
 
   $("#tuple").editable("click", function(e){
     try{
       tuple = JSON.parse(e.value);
+      if(url() !== location.href) location.href = url();
     }
     catch(ex){
       alert(e.value + " is not valid Tuple");
@@ -33,4 +35,12 @@ var log = function(msg){
   var watch_msg = $("<p>").text(msg).append(" - "+ new Date().toString());
   watch_msg.fadeIn(600);
   $("#watch").prepend(watch_msg);
+};
+
+var url = function(){
+  var _url = location.protocol+"//"+location.host+"/"+space_name;
+  for(var i = 0; i < tuple.length; i++){
+    if(tuple[i] && tuple[i].length > 0) _url += "/"+tuple[i];
+  };
+  return _url;
 };
